65370c78d65efc986b6e2f0f3522925a0e980c0a
jrpbjr <jrpbjr@gmail.com>
2024-08-01 14:18:34 -0300

    verifica se o usuario possue um tokem ou cookie

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 import { SignupUserRequest } from 'src/app/models/interfaces/user/SignupUserRequest';
 import { SignupUserResponse } from 'src/app/models/interfaces/user/SignupUserResponse';
@@ -13,7 +14,7 @@ import { environment } from 'src/environments/environment';
 export class UserService {
   private API_URL = environment.API_URL;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cookie: CookieService) {}
 
   signupUser(requestDatas: SignupUserRequest): Observable<SignupUserResponse> {
     return this.http.post<SignupUserResponse>(
@@ -25,4 +26,11 @@ export class UserService {
   authUser(requestDatas: AuthRequest): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.API_URL}/auth`, requestDatas);
   }
+
+  isLoggedIn():boolean{
+    // Verificar se o usuarioa  possui um token ou cookie
+    const JWT_TOKEN = this.cookie.get('USER_INFO');
+    return JWT_TOKEN ? true : false;
+  }
+
 }
