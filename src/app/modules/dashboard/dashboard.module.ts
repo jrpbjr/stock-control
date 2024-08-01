9c14b1fa0a63771783c2d48e0c1db3a7fd8b52a0
jrpbjr <jrpbjr@gmail.com>
2024-08-01 12:35:45 -0300

    criacao do modulo de dashboar-home

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DashboardHomeComponent } from './page/dashboard-home/dashboard-home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardHomeComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
