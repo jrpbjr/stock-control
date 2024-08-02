2279e5b29c85d728745ad6a1d426248328e37a41
jrpbjr <jrpbjr@gmail.com>
2024-08-02 08:58:45 -0300

    implementando shared.module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ToolbarModule } from 'primeng/toolbar';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { DialogService } from 'primeng/dynamicdialog';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    //PrimeNg
+    ToolbarModule,
+    CardModule,
+    ButtonModule
+  ],
+  providers: [DialogService, CurrencyPipe]
 })
 export class SharedModule { }
