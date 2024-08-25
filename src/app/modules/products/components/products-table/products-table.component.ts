62045025895ede4ea995b111a018d45a898af499
jrpbjr <jrpbjr@gmail.com>
2024-08-25 20:24:25 -0300

    implementando evento para botoes da tabela produtos e implementando outputs

--- src/app/modules/products/components/products-table/products-table.component.ts
+++ src/app/modules/products/components/products-table/products-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ProductEvent } from 'src/app/models/enums/products/ProductEvent';
+import { EventAction } from 'src/app/models/interfaces/products/event/EventAction'
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 
 @Component({
@@ -8,8 +10,18 @@ import { GetAllProductsResponse } from 'src/app/models/interfaces/products/respo
   styleUrls: []
 })
 export class ProductsTableComponent {
-@Input() products: Array<GetAllProductsResponse> = [];
+  @Input() products: Array<GetAllProductsResponse> = [];
+  @Output() productEvent = new EventEmitter<EventAction>();
 
-public productSelected!: GetAllProductsResponse;
+  public productSelected!: GetAllProductsResponse;
+  public addProductEvent = ProductEvent.ADD_PRODUCT_EVENT;
+  public editProductEvent = ProductEvent.EDIT_PRODUCT_EVENT;
 
+  handleProductEvent(action: string, id?: string): void {
+    if (action && action !== '') {
+      const productEventData = id && id !== '' ? { action, id } : { action };
+      // EMITIR O VALOR DO EVENTO
+      this.productEvent.emit(productEventData);
+    }
+  }
 }
--- src/app/modules/products/page/products-home/products-home.component.ts
+++ src/app/modules/products/page/products-home/products-home.component.ts
@@ -1,31 +1,27 @@
-import { ProductsService } from './../../../../services/products/products.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { MessageService } from 'primeng/api';
 import { Subject, takeUntil } from 'rxjs';
-import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+import { MessageService } from 'primeng/api';
+import { ProductsService } from '../../../../services/products/products.service';
 import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 
 @Component({
   selector: 'app-products-home',
   templateUrl: './products-home.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class ProductsHomeComponent implements OnInit, OnDestroy{
+export class ProductsHomeComponent implements OnInit, OnDestroy {
   private readonly destroy$: Subject<void> = new Subject();
-  public productsDatas: Array<GetAllProductsResponse> =[];
+  public productsDatas: Array<GetAllProductsResponse> = [];
 
   constructor(
     private productsService: ProductsService,
     private productsDtService: ProductsDataTransferService,
     private router: Router,
     private messageService: MessageService
-  ){}
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
+  ) {}
 
   ngOnInit(): void {
     this.getServiceProductsDatas();
@@ -34,35 +30,42 @@ export class ProductsHomeComponent implements OnInit, OnDestroy{
   getServiceProductsDatas() {
     const productsLoaded = this.productsDtService.getProductsDatas();
 
-    if (productsLoaded.length > 0){
+    if (productsLoaded.length > 0) {
       this.productsDatas = productsLoaded;
-    } else  this.getAPIProductsDatas();
-
-    //Apenas para teste
-    console.log('DADOS DE PRODUTOS', this.productsDatas);
+    } else this.getAPIProductsDatas();
   }
+
   getAPIProductsDatas() {
     this.productsService
-    .getAllProducts()
-    .pipe(takeUntil(this.destroy$))
-    .subscribe({
-      next: (response) => {
-        if (response.length > 0){
-          this.productsDatas = response;
-          //Apenas para teste
-          console.log('DADOS DE PRODUTOS', this.productsDatas);
-        }
-      },
-      error: (err) => {
-        console.log(err);
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Erro',
-          detail: 'Erro ao buscar produto',
-          life: 2500,
-        })
-        this.router.navigate(['/dashboard']);
-      }
-    })
+      .getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.productsDatas = response;
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar produtos',
+            life: 2500,
+          });
+          this.router.navigate(['/dashboard']);
+        },
+      });
+  }
+
+  handleProductAction(event: EventAction): void {
+    if (event) {
+      console.log('DADOS DO EVENTO RECEBIDO', event);
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
