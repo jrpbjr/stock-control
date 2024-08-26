fa93cc6e810feec39ff9537e891375569aaa6a46
jrpbjr <jrpbjr@gmail.com>
2024-08-26 13:51:16 -0300

    Inicio do form componente do produto

---
+++ src/app/modules/products/components/product-form/product-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-form',
+  templateUrl: './product-form.component.html',
+  //styleUrls: ['./product-form.component.scss']
+  styleUrls: []
+})
+export class ProductFormComponent {
+
+}
