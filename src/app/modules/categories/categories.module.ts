42c1566eff2b0fdcefeacc5ffb887d9bd0a5fb4e
jrpbjr <jrpbjr@gmail.com>
2024-09-08 21:43:33 -0300

    iniciando componente category para incluir e editar

--- src/app/modules/categories/categories.module.ts
+++ src/app/modules/categories/categories.module.ts
@@ -22,12 +22,14 @@ import { CategoriesHomeComponent } from './page/categories-home/categories-home.
 import { CATEGORIES_ROUTES } from './categories.routing';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CategoriesTableComponent } from './components/categories-table/categories-table.component';
+import { CategoryFormComponent } from './components/category-form/category-form.component';
 
 
 @NgModule({
   declarations: [
     CategoriesHomeComponent,
-    CategoriesTableComponent
+    CategoriesTableComponent,
+    CategoryFormComponent
   ],
   imports: [
     CommonModule,
