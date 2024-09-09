ae60e00404a6e15462342058c01fce50e8ffe972
jrpbjr <jrpbjr@gmail.com>
2024-09-09 19:45:09 -0300

    Implementando funcionalidade de adicionar categoria e mudando a posicao do metodo getallcategories para atualizacao da table

--- src/app/modules/categories/components/category-form/category-form.component.ts
+++ src/app/modules/categories/components/category-form/category-form.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+
+import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DynamicDialogConfig } from 'primeng/dynamicdialog';
+import { Subject, takeUntil } from 'rxjs';
+import { MessageService } from 'primeng/api';
+import { CategoriesService } from 'src/app/services/categories/categories.service';
+import { CategoryEvent } from 'src/app/models/enums/products/categories/CategoryEvent';
+import { EditCategoryAction } from 'src/app/models/interfaces/categories/event/EditCategoryAction';
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
-  styleUrls: ['./category-form.component.scss']
+  //styleUrls: ['./category-form.component.scss']
+  styleUrls: []
 })
-export class CategoryFormComponent {
+export class CategoryFormComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
 
+  public addCategoryAction = CategoryEvent.ADD_CATEGORY_ACTION;
+  public editCategoryAction = CategoryEvent.EDIT_CATEGORY_ACTION;
+
+  public categoryAction!: { event: EditCategoryAction };
+  public categoryForm = this.formBuilder.group({
+    name: ['', Validators.required],
+  });
+
+  constructor(
+    public ref: DynamicDialogConfig,
+    private formBuilder: FormBuilder,
+    private messageService: MessageService,
+    private categoriesService: CategoriesService
+  ) {}
+
+  ngOnInit(): void {
+    // this.cate
+  }
+
+  handleSubmitAddCategory(): void {
+    if (this.categoryForm?.value && this.categoryForm?.valid) {
+      const requestCreateCategory: { name: string } = {
+        name: this.categoryForm.value.name as string,
+      };
+
+      this.categoriesService
+        .createNewCategory(requestCreateCategory)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              this.categoryForm.reset();
+              this.messageService.add({
+                severity: 'success',
+                summary: 'Sucesso',
+                detail: 'Categoria criada com sucesso!',
+                life: 3000,
+              });
+            }
+          },
+          error: (err) => {
+            console.log(err);
+            this.categoryForm.reset();
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Erro ao criar categoria!',
+              life: 3000,
+            });
+          },
+        });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
